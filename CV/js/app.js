65d76e9ff7d76668c74b8985db5a59b9489804ea
ackseriam <ericka.simanca@gmail.com>
2021-08-27 18:00:57 -0400

    Cambios

--- CV/js/app.js
+++ CV/js/app.js
@@ -1,14 +1,15 @@
+/* Menu*/
 const menu = document.querySelector(".menu");
 
 const openMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
 
+
 openMenuBtn.onclick = () => {
-    info = document.getElementById("cont");
-    info.style.display = "none";
-    info.style.position = "none";
-    
-        
+  /*          
+    skill = document.getElementById("skills");
+    skill.style.display = "none";
+    info.style.position = "none";  */       
 }
 closeMenuBtn.onclick = () => {
     info =  document.getElementById("cont");
@@ -16,12 +17,15 @@ closeMenuBtn.onclick = () => {
 
     profile = document.getElementById("profile-img");
     profile.style.display = "block";
+
+    skill = document.getElementById("skills");
+    skill.style.display = "block";
 }
 
 function toggleMenu(){
     menu.classList.toggle("menu_opened");
     menu.classList.toggle("info");
-
+    
   
 }
 openMenuBtn.addEventListener("click", toggleMenu);
@@ -84,4 +88,49 @@ document.addEventListener('DOMContentLoaded', () =>{
 
 });
 
-/*Scrip */
+/* Skills efecto */
+
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const rightb = document.querySelector('.right::before')
+const skills = document.querySelector('.skillss')
+
+left.addEventListener('mouseenter', () => skills.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => skills.classList.hidden('hover-left'))
+
+right.addEventListener('mouseenter', () => skills.classList.add('hover-right'))
+right.addEventListener('mouseleave', () => skills.classList.remove('hover-right'))
+
+/* Slider Sobre mi*/
+const sliderContainer = document.querySelector('.slider-container')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const slidesLength = slideRight.querySelectorAll('div').length
+
+
+let activeSlideIndex = 0
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+    if(direction === 'up') {
+        activeSlideIndex++
+        if(activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+    } else if(direction === 'down') {
+        activeSlideIndex--
+        if(activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1
+        }
+    }
+
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+}
