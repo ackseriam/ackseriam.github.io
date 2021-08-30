c164c9a36241c3f424a5e1a22de4d5154c8e3037
ackseriam <ericka.simanca@gmail.com>
2021-08-30 19:25:56 -0400

    Cambios de fondo conteo de inicio

--- CV/js/app.js
+++ CV/js/app.js
@@ -1,10 +1,15 @@
 
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+const profile = document.querySelector('.profile-img')
+profile.style.display = 'none';
+const body = document.body;
+body.style.overflow= 'hidden';
+
 
 let load = 0
 
-let int = setInterval(blurring, 30)
+let int = setInterval(blurring, 5)
 
 function blurring() {
   load++
@@ -12,18 +17,24 @@ function blurring() {
   if (load > 99) {
     clearInterval(int)
     bg.style.background='none';
+    body.style.overflowX= 'hidden';
+    body.style.overflowY= 'auto';
+    profile.style.display = 'block';
+    bg.style.display='none';
 }
 
   loadText.innerText = `${load}%`
   loadText.style.opacity = scale(load, 0, 100, 1, 0)
   bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
- 
-  /*bg.remove();*/
+  
+    
+
 }
 
 // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+
 }
 
 /* Menu*/
