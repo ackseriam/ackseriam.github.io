e9f7b47c23ac5e975d3f29415320b9d804f38058
ackseriam <ericka.simanca@gmail.com>
2021-08-30 04:09:08 -0400

    Cambios de fondo conteo de inicio

--- CV/js/app.js
+++ CV/js/app.js
@@ -1,3 +1,31 @@
+
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
+
+let load = 0
+
+let int = setInterval(blurring, 30)
+
+function blurring() {
+  load++
+
+  if (load > 99) {
+    clearInterval(int)
+    bg.style.background='none';
+}
+
+  loadText.innerText = `${load}%`
+  loadText.style.opacity = scale(load, 0, 100, 1, 0)
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+ 
+  /*bg.remove();*/
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
+
 /* Menu*/
 const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
@@ -6,7 +34,6 @@ const openMenuBtn = document.querySelector(".open-menu");
 openMenuBtn.onclick = () => {
               
 }*/
-
 
 function toggleMenu(){
     menu.classList.toggle("move-left");
