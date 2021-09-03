2207031d08e9f65d2a23181bc2ae08ed715d75ad
ackseriam <ericka.simanca@gmail.com>
2021-09-03 12:26:14 -0400

    Cambios

--- CV/js/app.js
+++ CV/js/app.js
@@ -20,12 +20,58 @@ openMenuBtn.addEventListener("click", toggleMenu);
  el: ^= es para decir que todos los a herf que comience por #, "*/
 
  const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
-menuLinks.forEach(menuLink => {
+ const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => { /* con entries de la funcion intersection,
+        cuando una sesion este visible se agarra la id*/ 
+      const id =  entry.target.getAttribute("id"); /* se obtiene el id*/ 
+      /* se selecciona los a hrf que sean igual a # mas el nombre de la id */
+      const menuLink = document.querySelector(`.menu a[href="#${id}"]`);
+     
+      console.log(menuLink);
+      /* si se encuentra una interseaccion de un espacio se resalta el enlace perteneciente
+      a esa caja*/ 
+      if(entry.isIntersecting){
+        document.querySelector(".menu a.selected").classList.remove("selected");
+        menuLink.classList.add("selected");
+      } /*else{
+        menuLink.classList.remove("selected");
+      }*/          console.log(entries);
+    });
+  }, 
+  {rootMargin: "-30px 0px -70px 0px"}
+  
+  );
+
+
+/* aqui como estamos recorriendo todos los enlaces*/
+ menuLinks.forEach(menuLink => {
 menuLink.addEventListener("click", function(){
     menu.classList.remove("menu_opened");
     openMenuBtn.classList.toggle('open');
-  }) 
+  })
+
+const hash = menuLink.getAttribute("href");
+const target = document.querySelector(hash);
+
+if(target)
+{ 
+    observer.observe(target);
+}
+
 })
+
+/*
+/* aqui se selecciona el a href con el id de la seccion posicionada
+const hash = menuLink.getAttribute("href");
+/*en el se elige el hash  
+const target = document.querySelector(hash);
+if(target){ /* si existe el a href o la seleccion
+  se debe colocar dendro de observer  
+    observer.observe(target);
+}
+ */
+
 
 window.addEventListener("scroll",function()
 {
