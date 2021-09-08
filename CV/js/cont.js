6b63a3d39e278efaed5d2b966f9561d5bc510b41
ackseriam <ericka.simanca@gmail.com>
2021-09-07 21:48:47 -0400

    Cambios

--- CV/js/cont.js
+++ CV/js/cont.js
@@ -4,13 +4,17 @@ const loadText = document.querySelector('.loading-text');
 const bg = document.querySelector('.bg');
 const profiles = document.querySelector('#profiless');
 const fondo = document.querySelector('.fondo');
-fondo.style.zIndex = 50;
+/*fondo.style.zIndex = 50;
+*/
+/*const img = document.querySelector('#profile-img');
+img.style.zIndex = -1;
+const profile = document.querySelector('.profile-img')
+profile.style.display = 'none';
 
+*/
 const img = document.querySelector('#profile-img');
 img.style.zIndex = -1;
-
-/*const profile = document.querySelector('.profile-img')
-profile.style.display = 'none';*/
+const profile = document.querySelector('.profile-img');
 
 
 const body = document.body;
@@ -20,16 +24,18 @@ let int = setInterval(blurring, 40)
 
 function blurring() {
  load++
- /*const mul = load * '5'
- resul= mul;*/
+
   if (load > 99) {
     clearInterval(int)
     bg.style.background='none';
     body.style.overflowX= 'hidden';
     body.style.overflowY= 'auto';
     fondo.style.zIndex = -1;
-    profiles.style.zIndex = 1;
-    img.style.display = 'block';
+    loadText.style.display='none';
+ 
+  /*
+    profiles.style.zIndex = 1;     
+  img.style.display = 'block';
     /* 
      
   bg.style.display='none';*/
@@ -38,6 +44,7 @@ function blurring() {
 loadText.innerText = `${load}%`
 loadText.style.opacity = scale(load, 0, 100, 1, 0)
 bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+profiles.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
 // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
